250129e229771a1c6b322700bc4acff57a880ff6
v6854384 <32385043+v6854384@users.noreply.github.com>
2017-10-16 22:29:19 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream> 
+using namespace std; 
+
+int main() { 
+	int n, m, k ; 
+	cout << "Vvedite razmer massivov" << endl; 
+	cin >> n >> m; 
+	cout << "Vvedite stolbcy vtorogo massiva" << endl; 
+	cin >> k; 
+	int a[n][m], b[m][k], c[n][k]; 
+	for (int i = 0; i < n; i++) { 
+		for (int j = 0; j < m; j++) { 
+			cin » a[i][j]; 
+		} 
+	} 
+	cout << endl; 
+	for (int i = 0; i < m; i++) { 
+		for (int j = 0; j < k; j++) { 
+		cin >> b[i][j]; 
+		} 
+	} 
+	cout << endl; 
+	for (int i = 0; i<n;i++) { 
+		for (int j = 0;j<k;j++) { 
+		c[i][j] = 0; 
+		} 
+	} 
+
+	for (int i = 0; i < n; i++) { 
+		for (int j = 0; j < k; j++) { 
+			for (int r = 0; r < m; r++) { 
+			c[i][j] = c[i][j] + a[i][r] * b[r][j]; 
+
+		} 
+		cout << c[i][j] << " "; 
+		} 
+		cout << endl; 
+		} 
+}
